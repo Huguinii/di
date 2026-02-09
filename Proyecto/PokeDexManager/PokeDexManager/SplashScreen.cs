9eaa9dc3386373f2b041a3080eb6a6a2cbe93727
unknown <Diurno@C08PC26.informatica.comercio>
2026-02-09 12:22:13 +0100

    splash

---
+++ Proyecto/PokeDexManager/PokeDexManager/SplashScreen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokeDexManager
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < progressBar1.Maximum)
+            {
+                progressBar1.Value += 1;
+            }
+            else
+            {
+                timer1.Stop();
+
+                Form1 mainForm = new Form1();
+                mainForm.Show();
+
+                this.Hide();
+            }
+        }
+
+    }
+}
